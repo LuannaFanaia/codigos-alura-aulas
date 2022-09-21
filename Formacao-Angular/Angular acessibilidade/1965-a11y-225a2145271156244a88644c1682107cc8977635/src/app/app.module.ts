6962f2f7f0a55ffd90b09fe534a6616c50d2e386
LuannaFanaia <luannaff@hotmail.com>
2022-09-21 11:29:44 -0300

    Angular Acessibilidade concluído

--- Formacao-Angular/Angular acessibilidade/1965-a11y-225a2145271156244a88644c1682107cc8977635/src/app/app.module.ts
+++ Formacao-Angular/Angular acessibilidade/1965-a11y-225a2145271156244a88644c1682107cc8977635/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { DisableControlModule } from './shared/components/yes-no-button-group/directives/keyboard-manager/disable-control/disable-control.module';
 
 @NgModule({
     declarations: [AppComponent],
@@ -12,6 +13,7 @@ import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/
         YesNoButtonGroupModule,
         ReactiveFormsModule,
         FormsModule,
+        DisableControlModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
