4c6a1e2135fae54b87212f355100bd34a77c560a
LuannaFanaia <luannaff@hotmail.com>
2022-09-22 12:44:50 -0300

    Angular Acessibilidade - Metade do curso concluída

--- Formacao-Angular/Angular acessibilidade/Aula 02/src/app/app.component.ts
+++ Formacao-Angular/Angular acessibilidade/Aula 02/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+    ModalRef,
+    ModalService,
+} from './shared/components/modal/services/modal.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'a11y-p2';
+    @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
+
+    title = 'a11y-p2';
+    public firstName = 'Flávio';
+    public modalRef: ModalRef;
+
+    constructor(private modalService: ModalService) {}
+    public show(): void {
+        this.modalRef = this.modalService.open({
+            templateRef: this.modalTemplateRef,
+            title: 'User Details',
+        });
+    }
 }
