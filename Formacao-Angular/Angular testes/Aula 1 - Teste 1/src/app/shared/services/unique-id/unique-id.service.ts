28471a19c0a5543a6ff4119e6ed8c87425571ce5
LuannaFanaia <luannaff@hotmail.com>
2022-09-26 12:38:33 -0300

    Angular curso de testes - Erros no Karma

--- Formacao-Angular/Angular testes/Aula 1 - Teste 1/src/app/shared/services/unique-id/unique-id.service.ts
+++ Formacao-Angular/Angular testes/Aula 1 - Teste 1/src/app/shared/services/unique-id/unique-id.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UniqueIdService {
     private numberOfGenerateIds = 0;
 
     public generateUniqueIdWithPrefix(prefix: string): string {
+        if (!prefix) {
+            throw Error('Preifx cannot be empty');
+        }
         const uniqueId = this.generateUniqueId();
         this.numberOfGenerateIds++;
         return `${prefix}-$(uniqueId)`;
     }
 
+    public getNumberOfGenerateUniqueIds(): number {
+        return this.numberOfGenerateIds;
+    }
     private generateUniqueId(): string {
-        return null;
+        return uuidv4();
     }
 }
