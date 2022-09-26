c1902c285566f99e632f239e3cc05e652bd237d4
LuannaFanaia <luannaff@hotmail.com>
2022-09-26 08:59:00 -0300

    Angular Acessibilidade concluído

--- Formacao-Angular/Angular acessibilidade/Aula 02/src/app/app.module.ts
+++ Formacao-Angular/Angular acessibilidade/Aula 02/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from './shared/components/modal/modal.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         AppRoutingModule,
         ModalModule,
         BrowserAnimationsModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
