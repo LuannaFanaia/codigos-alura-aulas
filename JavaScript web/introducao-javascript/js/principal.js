5c8497d9be9d86a4053cff54a9bf4ebde1c1a50b
LuannaFanaia <luannaff@hotmail.com>
2022-11-01 13:04:36 -0300

    Estudando javascript

---
+++ JavaScript web/introducao-javascript/js/principal.js
@@ -0,0 +1,22 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+console.log(tdAltura);
+console.log(altura);
+console.log(imc);
